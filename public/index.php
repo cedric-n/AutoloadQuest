43765f4127f2cdfa496789fec77b5941ebfe5a21
cedric-n <cedric.ngondo@gmail.com>
2020-10-18 11:42:21 +0200

    take off addPsr method

--- public/index.php
+++ public/index.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Wcs\Hello;
-
 $loader = require __DIR__ . '/../vendor/autoload.php';
-$loader->addPsr4('App\\Wcs\\',__DIR__);
 
 
 $presentation = new Hello();
--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,5 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
+
 return ComposerAutoloaderInit78058868f268c6c746f3484600f44a00::getLoader();
